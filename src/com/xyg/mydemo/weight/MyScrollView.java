79fe7fa718b407aaa86ee77666ec1a3cb2c4f586
xyggun <2798904676@qq.com>
2014-10-11 17:46:23 +0800

    自己修改瀑布流

--- src/com/xyg/mydemo/weight/MyScrollView.java
+++ src/com/xyg/mydemo/weight/MyScrollView.java
@@ -1,12 +1,5 @@
 package com.xyg.mydemo.weight;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,7 +8,6 @@ import java.util.Set;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
@@ -24,13 +16,13 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.xyg.mydemo.R;
 import com.xyg.mydemo.common.ImageLoader;
+import com.xyg.mydemo.common.LoadImageTask;
 import com.xyg.mydemo.util.Images;
 
 @SuppressLint("ClickableViewAccessibility")
@@ -45,37 +37,7 @@ public class MyScrollView extends ScrollView implements OnTouchListener {
      * 记录当前已加载到第几页
      */
     private int page;
-
-    /**
-     * 每一列的宽度
-     */
-    private int columnWidth;
-
-    /**
-     * 当前第一列的高度
-     */
-    private int firstColumnHeight;
-
-    /**
-     * 当前第二列的高度
-     */
-    private int secondColumnHeight;
-
-    /**
-     * 当前第三列的高度
-     */
-    private int thirdColumnHeight;
-
-    /**
-     * 是否已加载过一次layout，这里onLayout中的初始化只需加载一次
-     */
-    private boolean loadOnce;
-
-    /**
-     * 对图片进行管理的工具类
-     */
-    private ImageLoader imageLoader;
-
+    
     /**
      * 第一列的布局
      */
@@ -90,6 +52,17 @@ public class MyScrollView extends ScrollView implements OnTouchListener {
      * 第三列的布局
      */
     private LinearLayout thirdColumn;
+
+    /**
+     * 是否已加载过一次layout，这里onLayout中的初始化只需加载一次
+     */
+    private boolean loadOnce;
+
+    /**
+     * 对图片进行管理的工具类
+     */
+    private ImageLoader imageLoader;
+
 
     /**
      * 记录所有正在下载或等待下载的任务。
@@ -169,7 +142,6 @@ public class MyScrollView extends ScrollView implements OnTouchListener {
                     firstColumn = (LinearLayout) findViewById(R.id.first_column);
                     secondColumn = (LinearLayout) findViewById(R.id.second_column);
                     thirdColumn = (LinearLayout) findViewById(R.id.third_column);
-                    columnWidth = firstColumn.getWidth();
                     loadOnce = true;
                     loadMoreImages();
             }
@@ -203,7 +175,7 @@ public class MyScrollView extends ScrollView implements OnTouchListener {
                                     endIndex = Images.imageUrls.length;
                             }
                             for (int i = startIndex; i < endIndex; i++) {
-                                    LoadImageTask task = new LoadImageTask();
+                                    LoadImageTask task = new LoadImageTask(getContext(),taskCollection,firstColumn,secondColumn,thirdColumn);
                                     taskCollection.add(task);
                                     task.execute(Images.imageUrls[i]);
                             }
@@ -233,7 +205,7 @@ public class MyScrollView extends ScrollView implements OnTouchListener {
                             if (bitmap != null) {
                                     imageView.setImageBitmap(bitmap);
                             } else {
-                                    LoadImageTask task = new LoadImageTask(imageView);
+                                    LoadImageTask task = new LoadImageTask(getContext(),imageView,imageLoader,taskCollection,firstColumn,secondColumn,thirdColumn);
                                     task.execute(imageUrl);
                             }
                     } else {
@@ -250,217 +222,6 @@ public class MyScrollView extends ScrollView implements OnTouchListener {
     private boolean hasSDCard() {
             return Environment.MEDIA_MOUNTED.equals(Environment
                             .getExternalStorageState());
-    }
-
-    /**
-     * 异步下载图片的任务。
-     * 
-     * @author guolin
-     */
-    class LoadImageTask extends AsyncTask<String, Void, Bitmap> {
-
-            /**
-             * 图片的URL地址
-             */
-            private String mImageUrl;
-
-            /**
-             * 可重复使用的ImageView
-             */
-            private ImageView mImageView;
-
-            public LoadImageTask() {
-            }
-
-            /**
-             * 将可重复使用的ImageView传入
-             * 
-             * @param imageView
-             */
-            public LoadImageTask(ImageView imageView) {
-                    mImageView = imageView;
-            }
-
-            @Override
-            protected Bitmap doInBackground(String... params) {
-                    mImageUrl = params[0];
-                    Bitmap imageBitmap = imageLoader
-                                    .getBitmapFromMemoryCache(mImageUrl);
-                    if (imageBitmap == null) {
-                            imageBitmap = loadImage(mImageUrl);
-                    }
-                    return imageBitmap;
-            }
-
-            @Override
-            protected void onPostExecute(Bitmap bitmap) {
-                    if (bitmap != null) {
-                            double ratio = bitmap.getWidth() / (columnWidth * 1.0);
-                            int scaledHeight = (int) (bitmap.getHeight() / ratio);
-                            addImage(bitmap, columnWidth, scaledHeight);
-                    }
-                    taskCollection.remove(this);
-            }
-
-            /**
-             * 根据传入的URL，对图片进行加载。如果这张图片已经存在于SD卡中，则直接从SD卡里读取，否则就从网络上下载。
-             * 
-             * @param imageUrl
-             *            图片的URL地址
-             * @return 加载到内存的图片。
-             */
-            private Bitmap loadImage(String imageUrl) {
-                    File imageFile = new File(getImagePath(imageUrl));
-                    if (!imageFile.exists()) {
-                            downloadImage(imageUrl);
-                    }
-                    if (imageUrl != null) {
-                            Bitmap bitmap = ImageLoader.decodeSampledBitmapFromResource(
-                                            imageFile.getPath(), columnWidth);
-                            if (bitmap != null) {
-                                    imageLoader.addBitmapToMemoryCache(imageUrl, bitmap);
-                                    return bitmap;
-                            }
-                    }
-                    return null;
-            }
-
-            /**
-             * 向ImageView中添加一张图片
-             * 
-             * @param bitmap
-             *            待添加的图片
-             * @param imageWidth
-             *            图片的宽度
-             * @param imageHeight
-             *            图片的高度
-             */
-            private void addImage(Bitmap bitmap, int imageWidth, int imageHeight) {
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                                    imageWidth, imageHeight);
-                    if (mImageView != null) {
-                            mImageView.setImageBitmap(bitmap);
-                    } else {
-                            ImageView imageView = new ImageView(getContext());
-                            imageView.setLayoutParams(params);
-                            imageView.setImageBitmap(bitmap);
-                            imageView.setScaleType(ScaleType.FIT_XY);
-                            imageView.setPadding(5, 5, 5, 5);
-                            imageView.setTag(R.string.image_url, mImageUrl);
-                            findColumnToAdd(imageView, imageHeight).addView(imageView);
-                            imageViewList.add(imageView);
-                    }
-            }
-
-            /**
-             * 找到此时应该添加图片的一列。原则就是对三列的高度进行判断，当前高度最小的一列就是应该添加的一列。
-             * 
-             * @param imageView
-             * @param imageHeight
-             * @return 应该添加图片的一列
-             */
-            private LinearLayout findColumnToAdd(ImageView imageView,
-                            int imageHeight) {
-                    if (firstColumnHeight <= secondColumnHeight) {
-                            if (firstColumnHeight <= thirdColumnHeight) {
-                                    imageView.setTag(R.string.border_top, firstColumnHeight);
-                                    firstColumnHeight += imageHeight;
-                                    imageView.setTag(R.string.border_bottom, firstColumnHeight);
-                                    return firstColumn;
-                            }
-                            imageView.setTag(R.string.border_top, thirdColumnHeight);
-                            thirdColumnHeight += imageHeight;
-                            imageView.setTag(R.string.border_bottom, thirdColumnHeight);
-                            return thirdColumn;
-                    } else {
-                            if (secondColumnHeight <= thirdColumnHeight) {
-                                    imageView.setTag(R.string.border_top, secondColumnHeight);
-                                    secondColumnHeight += imageHeight;
-                                    imageView
-                                                    .setTag(R.string.border_bottom, secondColumnHeight);
-                                    return secondColumn;
-                            }
-                            imageView.setTag(R.string.border_top, thirdColumnHeight);
-                            thirdColumnHeight += imageHeight;
-                            imageView.setTag(R.string.border_bottom, thirdColumnHeight);
-                            return thirdColumn;
-                    }
-            }
-
-            /**
-             * 将图片下载到SD卡缓存起来。
-             * 
-             * @param imageUrl
-             *            图片的URL地址。
-             */
-            private void downloadImage(String imageUrl) {
-                    HttpURLConnection con = null;
-                    FileOutputStream fos = null;
-                    BufferedOutputStream bos = null;
-                    BufferedInputStream bis = null;
-                    File imageFile = null;
-                    try {
-                            URL url = new URL(imageUrl);
-                            con = (HttpURLConnection) url.openConnection();
-                            con.setConnectTimeout(5 * 1000);
-                            con.setReadTimeout(15 * 1000);
-                            con.setDoInput(true);
-                            con.setDoOutput(true);
-                            bis = new BufferedInputStream(con.getInputStream());
-                            imageFile = new File(getImagePath(imageUrl));
-                            fos = new FileOutputStream(imageFile);
-                            bos = new BufferedOutputStream(fos);
-                            byte[] b = new byte[1024];
-                            int length;
-                            while ((length = bis.read(b)) != -1) {
-                                    bos.write(b, 0, length);
-                                    bos.flush();
-                            }
-                    } catch (Exception e) {
-                            e.printStackTrace();
-                    } finally {
-                            try {
-                                    if (bis != null) {
-                                            bis.close();
-                                    }
-                                    if (bos != null) {
-                                            bos.close();
-                                    }
-                                    if (con != null) {
-                                            con.disconnect();
-                                    }
-                            } catch (IOException e) {
-                                    e.printStackTrace();
-                            }
-                    }
-                    if (imageFile != null) {
-                            Bitmap bitmap = ImageLoader.decodeSampledBitmapFromResource(
-                                            imageFile.getPath(), columnWidth);
-                            if (bitmap != null) {
-                                    imageLoader.addBitmapToMemoryCache(imageUrl, bitmap);
-                            }
-                    }
-            }
-
-            /**
-             * 获取图片的本地存储路径。
-             * 
-             * @param imageUrl
-             *            图片的URL地址。
-             * @return 图片的本地存储路径。
-             */
-            private String getImagePath(String imageUrl) {
-                    int lastSlashIndex = imageUrl.lastIndexOf("/");
-                    String imageName = imageUrl.substring(lastSlashIndex + 1);
-                    String imageDir = Environment.getExternalStorageDirectory()
-                                    .getPath() + "/PhotoWallFalls/";
-                    File file = new File(imageDir);
-                    if (!file.exists()) {
-                            file.mkdirs();
-                    }
-                    String imagePath = imageDir + imageName;
-                    return imagePath;
-            }
     }
 
 }
