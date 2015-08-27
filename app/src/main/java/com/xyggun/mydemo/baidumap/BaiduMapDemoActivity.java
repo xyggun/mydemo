95d5902185807e1c37eea96be8f69676c96906df
xyggun <2798904676@qq.com>
2015-08-27 22:27:28 +0800

    添加百度地图定位功能

--- app/src/main/java/com/xyggun/mydemo/baidumap/BaiduMapDemoActivity.java
+++ app/src/main/java/com/xyggun/mydemo/baidumap/BaiduMapDemoActivity.java
@@ -14,7 +14,17 @@ import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 import com.xyggun.baidumaplibrary.MyLocationListener;
 import com.xyggun.baselibrary.inject.InjectView;
 import com.xyggun.baselibrary.inject.SetContentView;
@@ -37,18 +47,21 @@ public class BaiduMapDemoActivity extends BaseActivity implements View.OnClickLi
     Button btnMapTrafficEnabled;
     @InjectView(R.id.btn_heat_map_enabled)
     Button btnMapHeatEnabled;
-//    @InjectView(R.id.btn_location)
+    @InjectView(R.id.btn_location)
     Button btnMapLocation;
 
     BaiduMap mBaiduMap;
     Dialog WaitDialog;
-    AlertDialog MessageDialog;
     AlertDialog.Builder MessageDialogBuilder;
+
+    // 定位
+    BitmapDescriptor mCurrentMarker;
+    MyLocationConfiguration.LocationMode mCurrentMode;
 
     public LocationClient mLocationClient = null;
     public BDLocationListener myListener = new BDLocationListener() {
         @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
+        public void onReceiveLocation(final BDLocation bdLocation) {
             StringBuffer sb = MyLocationListener.getBDString(bdLocation);
             dismssDialog(WaitDialog);
             MessageDialogBuilder = DialogHelp.getMessageDialog(BaiduMapDemoActivity.this, sb.toString(),
@@ -59,7 +72,9 @@ public class BaiduMapDemoActivity extends BaseActivity implements View.OnClickLi
                         }
                     });
             MessageDialogBuilder.show();
-
+            markedPoint(bdLocation.getLatitude(),bdLocation.getLongitude());
+            setMapStatus(bdLocation);
+            mLocationClient.stop();
         }
     };
 
@@ -123,7 +138,7 @@ public class BaiduMapDemoActivity extends BaseActivity implements View.OnClickLi
         option.setLocationMode(com.baidu.location.LocationClientOption.LocationMode.Hight_Accuracy
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span = 1000;
+        int span = 0;
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true);//可选，默认false,设置是否使用gps
@@ -134,6 +149,69 @@ public class BaiduMapDemoActivity extends BaseActivity implements View.OnClickLi
         option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
         option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
         mLocationClient.setLocOption(option);
+    }
+
+    /**
+     * 设置location
+     * @param bdLocation
+     */
+    private void setLocation(BDLocation bdLocation){
+
+
+        mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        // 构造定位数据
+        MyLocationData locData = new MyLocationData.Builder()
+                .accuracy(bdLocation.getRadius())
+                        // 此处设置开发者获取到的方向信息，顺时针0-360
+                .direction(100).latitude(bdLocation.getLatitude())
+                .longitude(bdLocation.getLongitude()).build();
+        // 设置定位数据
+        mBaiduMap.setMyLocationData(locData);
+        // 设置定位图层的配置（定位模式，是否允许方向信息，用户自定义定位图标）
+        mCurrentMarker = BitmapDescriptorFactory
+                .fromResource(R.drawable.loading_9);
+        MyLocationConfiguration config = new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker);
+        mBaiduMap.setMyLocationConfigeration(config);
+        // 当不需要定位图层时关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
+    }
+
+    /**
+     * 标记坐标点
+     * @param latutyde
+     * @param longitude
+     */
+    private void markedPoint(double latutyde,double longitude){
+        //定义Maker坐标点
+        LatLng point = new LatLng(latutyde, longitude);
+        //构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.drawable.loading_9);
+        //构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions()
+                .position(point)
+                .icon(bitmap);
+        //在地图上添加Marker，并显示
+        mBaiduMap.addOverlay(option);
+    }
+
+    private void setMapStatus(BDLocation bdLocation){
+        // 设定中心点坐标
+        LatLng cenpt =  new LatLng(bdLocation.getLatitude(),bdLocation.getLongitude());
+
+
+        // 定义地图状态
+        MapStatus mMapStatus = new MapStatus.Builder()
+                .target(cenpt)
+                .zoom(16)
+                .build();
+
+        // 定义MapStatusUpdate对象，以便描述地图状态将要发生的变化
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+        // 改变地图状态
+        mBaiduMap.setMapStatus(mMapStatusUpdate);
     }
 
     @Override
@@ -159,12 +237,12 @@ public class BaiduMapDemoActivity extends BaseActivity implements View.OnClickLi
                 mBaiduMap.setBaiduHeatMapEnabled(true);
             }
         }
-//        if (id == R.id.btn_location) {
-//            WaitDialog = DialogHelp.getWaitDialog(this, "定位中...");
-//            WaitDialog.show();
-//            initLocation();
-//            mLocationClient.start();
-//        }
+        if (id == R.id.btn_location) {
+            WaitDialog = DialogHelp.getWaitDialog(this, "定位中...");
+            WaitDialog.show();
+            initLocation();
+            mLocationClient.start();
+        }
         toggleRadioMenu();
     }
 
