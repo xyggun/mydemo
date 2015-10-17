dbc9d5483a48832778a03ef37fa4c8c1cab44303
xyggun <2798904676@qq.com>
2015-10-17 11:30:16 +0800

    添加返回的字体变化

---
+++ app/src/main/java/com/xyggun/mydemo/ui/HandleTestActivity.java
@@ -0,0 +1,18 @@
+package com.xyggun.mydemo.ui;
+
+import android.os.Bundle;
+
+import com.xyggun.baselibrary.base.BaseActivity;
+import com.xyggun.mydemo.R;
+/**
+ * handle 测试类
+ */
+public class HandleTestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handle_test);
+    }
+
+}
